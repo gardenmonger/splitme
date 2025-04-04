c5e792f7a7a74318978b03c0f46822597b3925ad
gardenmonger <leejeanty1@yahoo.com>
2025-04-04 04:44:13 +0000

    Added the main function

---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from pitchDetect import AudioPitchDetector
+from audioSplitter import AudioTransientSplitter
+import GetFile as file
+import GetOutput as folder
+
+def main():
+    print("Main Function")
+    audio = file.get_file_path()
+    output = folder.get_folder_path()
+    sound = AudioPitchDetector(audio)
+    splitter = AudioTransientSplitter()
+    
+    # Visualize the transients
+    splitter.visualize_transients(audio)
+
+    output_files = splitter.split_audio(
+        audio,
+        output,
+        min_duration_sec=0.1,
+        offset_ms=20)
+    # Adjust detection parameters for different audio characteristics
+    splitter.adjust_parameters(
+        delta=0.05,            # Lower threshold for more sensitive detection
+        wait=15                # Shorter wait period between transients
+    )
+    print(sound.detect_pitch())
+
+
+
+if __name__ == '__main__':
+    main()
